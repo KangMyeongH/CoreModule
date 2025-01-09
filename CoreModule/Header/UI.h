57b3c1e87cf5c652cfe25b73bb7748bed0a77f69
KangMyeongH <71129440+KangMyeongH@users.noreply.github.com>
2025-01-09 23:12:01 +0900

    [bugfix] native-scale fix
    1. native-scale을 구할때 원본 이미지의 해상도를 구하지 않고 디바이스가 변형한 이미지의 해상도를 구하는 문제 수정

--- CoreModule/Header/UI.h
+++ CoreModule/Header/UI.h
@@ -5,10 +5,19 @@ namespace GameEngine
 	class UI : public Behaviour
 	{
 	public:
-		UI() : Behaviour(nullptr)
-		{}
-		explicit UI(GameObject* _owner) : Behaviour(_owner)
-		{}
+		UI() : Behaviour(nullptr), m_Texture(nullptr)
+		{
+		}
+
+		explicit UI(GameObject* _owner) : Behaviour(_owner), m_Texture(nullptr)
+		{
+		}
+
+		~UI() override;
+
+	private:
+		std::wstring		m_Path;
+		LPDIRECT3DTEXTURE9	m_Texture;
 	};
 }
 
